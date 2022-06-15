9c8b61dc9274f6244f7f28140b80187141f6459d
Jahill17 <104641021+Jahill17@users.noreply.github.com>
2022-06-15 16:52:59 -0400

    update sql retirement table

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -96,3 +96,13 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Create new table for retiring employees after dropping old retirement table
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
